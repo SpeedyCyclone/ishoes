d7d80ab1053a7a26b3e505ecd99c1360014cb704
JuniorHat123 <74607492+SpeedyCyclone@users.noreply.github.com>
2023-07-24 17:43:32 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-const cronJob = require("../../cronJob");
 
 export default function Home() {
   return (
